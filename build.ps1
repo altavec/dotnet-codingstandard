2007caa29421a89860b5a96f2a0bb21d318cc3c8
Ross King <ross.king@altavec.com>
2024-07-29 12:16:32 +1000

    💚 ignore invalid NuGet warning

--- build.ps1
+++ build.ps1
@@ -5,4 +5,4 @@ dotnet build .\tasks\Altavec.DotNet.CodingStandard.Tasks --configuration Release
 $sha = (git rev-parse HEAD)
 $ref_name = (git symbolic-ref --short HEAD)
 $repositoryUrl = (git remote get-url origin)
-nuget pack Altavec.DotNet.CodingStandard.nuspec -ForceEnglishOutput -Version $version -Properties "RepositoryCommit=$sha;RepositoryBranch=$ref_name;RepositoryUrl=$repositoryUrl" -Properties "NoWarn=NU5100"
+nuget pack Altavec.DotNet.CodingStandard.nuspec -ForceEnglishOutput -Version $version -Properties "RepositoryCommit=$sha;RepositoryBranch=$ref_name;RepositoryUrl=$repositoryUrl;NoWarn=NU5100"
