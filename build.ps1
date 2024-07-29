925b2f2a16ecdf73c540049677f901eb3fb3a7d0
Ross King <ross.king@altavec.com>
2024-07-29 12:45:19 +1000

    💚 fix test issue

--- build.ps1
+++ build.ps1
@@ -1,8 +1,9 @@
 $version = '777.77.7'
+$configuration = 'Release'
 
-dotnet build .\tasks\Altavec.DotNet.CodingStandard.Tasks --configuration Release -property:Version=$version
+dotnet build .\tasks\Altavec.DotNet.CodingStandard.Tasks --configuration $configuration -property:Version=$version
 
 $sha = (git rev-parse HEAD)
 $ref_name = (git symbolic-ref --short HEAD)
 $repositoryUrl = (git remote get-url origin)
-nuget pack Altavec.DotNet.CodingStandard.nuspec -ForceEnglishOutput -Version $version -Properties "RepositoryCommit=$sha;RepositoryBranch=$ref_name;RepositoryUrl=$repositoryUrl;NoWarn=NU5100"
+nuget pack Altavec.DotNet.CodingStandard.nuspec -ForceEnglishOutput -Version $version -Properties "RepositoryCommit=$sha;RepositoryBranch=$ref_name;RepositoryUrl=$repositoryUrl;NoWarn=NU5100;Configuration=$configuration"
